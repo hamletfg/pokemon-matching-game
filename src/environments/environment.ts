dda1c9a812dfc2f533efdc271679fe67ce6830f7
Hamlet Gordillo <hamletfg@gmail.com>
2025-10-06 21:57:28 -0600

    Add initial setup files along with firebase setup.

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCYe2RQmOyV5g1NhkA2SybVFEzWRYqQFVM',
+    authDomain: 'pokemon-matching-game-81828.firebaseapp.com',
+    projectId: 'pokemon-matching-game-81828',
+    storageBucket: 'pokemon-matching-game-81828.firebasestorage.app',
+    messagingSenderId: '884427391331',
+    appId: '1:884427391331:web:9410997470f5942a181356',
+  },
+};
