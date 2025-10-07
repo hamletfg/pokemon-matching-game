68d3a03ede2bfb6af4974a724c0af6e807997f7b
Hamlet Gordillo <hamletfg@gmail.com>
2025-10-07 11:32:11 -0600

    Add toolbar module to app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar'; // <-- Add this import
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    MatToolbarModule, // <-- Add this to the imports array
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('pokemon-matching-game');
+export class AppComponent {
+  title = 'pokemon-matching-game';
 }
