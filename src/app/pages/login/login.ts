c8f768244add33cdb2135a4d542107f3f7f13d2e
Hamlet Gordillo <hamletfg@gmail.com>
2025-10-07 15:57:40 -0600

    Generate core folder

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule], // <-- Import Material modules
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrl: './login.scss',
 })
 export class Login {
-
+  login() {
+    console.log('Login button clicked!');
+  }
 }
