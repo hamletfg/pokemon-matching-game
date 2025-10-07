9996104ecb46bf7eba6148e1360282fb06a038a9
Hamlet Gordillo <hamletfg@gmail.com>
2025-10-07 11:29:43 -0600

    Add routes to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Import the new components
+import { LoginComponent } from './pages/login/login.component';
+import { GameSetupComponent } from './pages/game-setup/game-setup.component';
+import { GameBoardComponent } from './pages/game-board/game-board.component';
+import { UserStatsComponent } from './pages/user-stats/user-stats.component';
+
+export const routes: Routes = [
+  // When the app loads, redirect the empty path to the login page
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+
+  // Define the path for each page
+  { path: 'login', component: LoginComponent },
+  { path: 'setup', component: GameSetupComponent },
+  { path: 'game', component: GameBoardComponent },
+  { path: 'stats', component: UserStatsComponent },
+];
